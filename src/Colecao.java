821697876ae7cf83756f138eeab5ecd660e1416a
Pedro Lucas <pedro.borges4@ufu.br>
2024-11-03 14:01:37 -0300

    Quase lá

--- src/Colecao.java
+++ src/Colecao.java
@@ -40,6 +40,7 @@ public class Colecao extends Biblioteca {
         }
     }
 
+    //setDescricao
     public void mudarDescricao(String descricaoNova) {
 
         this.descricaoColecao = descricaoNova;
